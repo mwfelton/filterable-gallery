719835d037f8abca529cb192700b99ab8da876fa
mwfelton <72751046+mwfelton@users.noreply.github.com>
2021-10-11 13:45:21 +0100

    Transition added
    Cards now come and go with a transition

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 const controlButton = document.querySelectorAll('.control')
 const card = document.querySelectorAll('.card')
-const divs = document.querySelectorAll('div')
 
 controlButton.forEach(button => button.addEventListener('click', (e) => {
 
-    card.forEach(card => card.style.display = "none")
     const att = button.getAttribute("data-location")
-
-    for (let i = 0; i <= divs.length; i++){
-        if (divs[i].classList.contains(`${att}`)){
-            divs[i].style.display = 'block'
+    for (let i = 0; i <= card.length; i++){
+        if (!card[i].classList.contains(`${att}`)){
+            card[i].classList.add('fade-out')
+            setTimeout(() => {
+                card[i].style.display = 'none'
+            }, 300);
+        } else {
+            card[i].classList.remove('fade-out')
+            setTimeout(() => {
+                card[i].style.display = 'block'
+            }, 300);
         }
     }
+
 }))
+
+
+
+
+
+//old -----------
+
+// controlButton.forEach(button => button.addEventListener('click', (e) => {
+
+//     card.forEach(card => card.style.display = "none")
+//     const att = button.getAttribute("data-location")
+
+//     for (let i = 0; i <= divs.length; i++){
+//         if (divs[i].classList.contains(`${att}`)){
+//             divs[i].style.display = 'block'
+//         }
+//     }
+// }))
