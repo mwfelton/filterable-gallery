a20ed72890d7b1363353bb03a6ad30b29bcfb6ce
Miles Felton Gerber <mwfelton03@gmail.com>
2021-10-12 15:05:54 +0100

    fully animated using web-kit
    I was able to acheive the animation I wanted however the use of web-kit has seriously limited the apps cross browser compatibility

--- app.js
+++ app.js
@@ -6,34 +6,9 @@ controlButton.forEach(button => button.addEventListener('click', (e) => {
     const att = button.getAttribute("data-location")
     for (let i = 0; i <= card.length; i++){
         if (!card[i].classList.contains(`${att}`)){
-            card[i].classList.add('fade-out')
-            setTimeout(() => {
-                card[i].style.display = 'none'
-            }, 300);
+            card[i].classList.add("hidden")
         } else {
-            card[i].classList.remove('fade-out')
-            setTimeout(() => {
-                card[i].style.display = 'block'
-            }, 300);
+            card[i].classList.remove("hidden")
         }
     }
-
 }))
-
-
-
-
-
-//old -----------
-
-// controlButton.forEach(button => button.addEventListener('click', (e) => {
-
-//     card.forEach(card => card.style.display = "none")
-//     const att = button.getAttribute("data-location")
-
-//     for (let i = 0; i <= divs.length; i++){
-//         if (divs[i].classList.contains(`${att}`)){
-//             divs[i].style.display = 'block'
-//         }
-//     }
-// }))
